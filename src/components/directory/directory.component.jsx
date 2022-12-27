b2e46045aa665b490fbb03a1ed503ae29bb9e02d
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2022-12-27 13:08:15 +0100

    Homepage into smaller components and also finished styling the menu items

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "Hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+        },
+        {
+          title: "Jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+        },
+        {
+          title: "Sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+        },
+        {
+          title: "Womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+        },
+        {
+          title: "Mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div
+      //className="menu-item"
+      className={`menu-item ${size}`}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url('${imageUrl}')`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">Shop now!!!</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
