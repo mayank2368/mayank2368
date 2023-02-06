01e1cd0b599d7257437b8dc935358ed6f203c155
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-02-06 13:55:39 +0100

    integrated stripe payments and updated the favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey =
+    "pk_test_51MYTHOAwGS6o4vlrsBh8Ce9Pbg3YtHEy3BbFpmY4WxFaeQfh8YAEkF3GuoeLAZKhpEfvp5kDzeWMeitq7v5jTlIC00MHfnUMMJ";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Shopify"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import "./checkout.styles.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import {
   selectCartItems,
@@ -33,9 +34,12 @@ const Checkout = ({ cartItems, total }) => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className="total">
-        <span>TOTAL: ${total}</span>
+      <div className="total">TOTAL: ${total}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments* <br />
+        4242 4242 4242 4242 - Exp: 01/02 - CVV: 123
       </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
