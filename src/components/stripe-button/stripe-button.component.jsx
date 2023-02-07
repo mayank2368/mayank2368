73f83c1bd2991e5a21fdfa46052c1c1402d4be25
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-02-07 14:37:40 +0100

    Upgraded the logger just to the development env

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -14,7 +14,7 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label="Pay Now"
-      name="Shopify"
+      name="Crwn Clothing Ltd."
       billingAddress
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import rootReducer from "./root-reducer";
 //import { combineReducers } from "redux";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 export const store = configureStore(
   rootReducer,
   applyMiddleware(...middlewares)
